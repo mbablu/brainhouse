15cde25a4f4e0d85a509d003c8153ffa8d1dcfcc
mbablu <mbbiswasbd@yahoo.com>
2025-07-17 23:18:30 +0600

    established year change

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -211,7 +211,7 @@ export default function AboutUs() {
                   <path fill="currentColor" d="M96 32V64H48C21.5 64 0 85.5 0 112v48H448V112c0-26.5-21.5-48-48-48H352V32c0-17.7-14.3-32-32-32s-32 14.3-32 32V64H160V32c0-17.7-14.3-32-32-32S96 14.3 96 32zM448 192H0V464c0 26.5 21.5 48 48 48H400c26.5 0 48-21.5 48-48V192z"></path>
                 </svg>
               </div>
-              <h3 className="text-2xl lg:text-3xl font-bold text-[#192DA0] ">2026</h3>
+              <h3 className="text-2xl lg:text-3xl font-bold text-[#192DA0] ">2016</h3>
               <p className="text-gray-600">Established</p>
             </div>
             <div id="location-stat" className="text-center">
